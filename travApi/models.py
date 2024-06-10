9535a2277c30c9a44127a749959471027abd73cd
codeedexpython <codeedexpython@gmail.com>
2024-06-10 11:07:07 +0530

    first

--- travApi/models.py
+++ travApi/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class Vehicle(models.Model):
+    vehicle_id=models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vehicle_type = models.CharField(max_length=50)
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.CharField(max_length=4)
+    license_plate = models.CharField(max_length=20, unique=True)
+    color = models.CharField(max_length=30)
+    seats_available = models.PositiveIntegerField()
+    registration_document = models.URLField()
+
+    class Meta:
+        db_table="vehicle_table"
+class Trip(models.Model):
+    trip_id=models.AutoField(primary_key=True)
+    driver = models.ForeignKey(User,on_delete=models.CASCADE)
+    vehicle_id= models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    start_location = models.CharField(max_length=255)
+    end_location = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    available_seats = models.IntegerField()
+    fare = models.IntegerField()
+    class Meta:
+        db_table="trip_table"
+
+class Booking(models.Model):
+    booking_id=models.AutoField(primary_key=True)
+    trip_id= models.ForeignKey(Trip, on_delete=models.CASCADE)
+    passenger= models.ForeignKey(User, on_delete=models.CASCADE)
+    seats_booked = models.IntegerField()
+    booking_time = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default='pending')
+
+    class Meta:
+        db_table = "booking_table"
+
+
--- travelista/settings.py
+++ travelista/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'travApi'
+    'travApi','rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'travelista.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'trav_db',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'PORT': ''
     }
 }
 
