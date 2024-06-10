c2bddb2590ac009a38f97b120fba0e016e80ccc1
Fayas <fayaspnisthar@gmail.com>
2024-06-10 15:22:20 +0530

    models

--- travApi/models.py
+++ travApi/models.py
@@ -58,6 +58,47 @@ class RideAndDrive(models.Model):
 
     def __str__(self):
         return self.user_type
+    
+
+class Vehicle(models.Model):
+    vehicle_id=models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vehicle_type = models.CharField(max_length=50)
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.CharField(max_length=4)
+    license_plate = models.CharField(max_length=20, unique=True)
+    color = models.CharField(max_length=30)
+    seats_available = models.PositiveIntegerField()
+    registration_document = models.URLField()
+
+    class Meta:
+        db_table="vehicle_table"
+
+class Trip(models.Model):
+    trip_id=models.AutoField(primary_key=True)
+    driver = models.ForeignKey(User,on_delete=models.CASCADE)
+    vehicle_id= models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    start_location = models.CharField(max_length=255)
+    end_location = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    available_seats = models.IntegerField()
+    fare = models.IntegerField()
+    
+    class Meta:
+        db_table="trip_table"
+
+class Booking(models.Model):
+    booking_id=models.AutoField(primary_key=True)
+    trip_id= models.ForeignKey(Trip, on_delete=models.CASCADE)
+    passenger= models.ForeignKey(User, on_delete=models.CASCADE)
+    seats_booked = models.IntegerField()
+    booking_time = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default='pending')
+
+    class Meta:
+        db_table = "booking_table"
 
 
 @receiver(post_save, sender=User)
