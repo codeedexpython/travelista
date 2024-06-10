4cb3ccd1f96d79d36889e8c0e71ef61e3bab847a
Fayas <fayaspnisthar@gmail.com>
2024-06-10 10:48:41 +0530

    user registration

--- travApi/models.py
+++ travApi/models.py
@@ -1,3 +1,70 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.utils import timezone
+from django.core.validators import RegexValidator,MaxValueValidator, MinValueValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone_number = models.CharField(
+        max_length=10,
+        validators=[RegexValidator(r'^[6-9]\d{9}$')]
+    )
+    profile_picture = models.ImageField(upload_to='profile_pictures/')
+    address = models.CharField(max_length=250)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.phone_number
+    
+class RideAndDrive(models.Model):
+    USER_TYPES = [
+        ('user', 'User'),
+        ('driver', 'Driver'),
+        ('both', 'Both'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=10, choices=USER_TYPES, default='user')
+    vehicle_information = models.TextField(null=True, blank=True)
+    driver_license = models.CharField(max_length=30, null=True, blank=True)
+    license_expiration_date = models.DateField(null=True, blank=True)
+    driver_rating = models.DecimalField(
+        max_digits=2, decimal_places=1, null=True, blank=True,
+        validators=[MinValueValidator(1.0), MaxValueValidator(5.0)]
+    )
+    rider_rating = models.DecimalField(
+        max_digits=2, decimal_places=1, null=True, blank=True,
+        validators=[MinValueValidator(1.0), MaxValueValidator(5.0)]
+    )
+    availability_status = models.BooleanField(default=True)
+    preferred_payment_method = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.user_type
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- travApi/views.py
+++ travApi/views.py
@@ -1,3 +1,128 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Profile
+from .serializers import *
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserCreateView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProfileUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        try:
+            profile = Profile.objects.get(user=request.user)
+            serializer = ProfileSerializer(instance=profile)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            return Response({"detail": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, *args, **kwargs):
+        try:
+            profile = Profile.objects.get(user=request.user)
+            serializer = ProfileSerializer(instance=profile, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Profile.DoesNotExist:
+            return Response({"detail": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+class PasswordChangeView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def post(self, request, *args, **kwargs):
+        serializer = PasswordChangeSerializer(data=request.data, context={'user': request.user})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"detail": "Password changed successfully."}, status=status.HTTP_200_OK)
+        return Response({"detail": "Invalid input data for password change"}, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data.get('email')
+            password = serializer.validated_data.get('password')
+
+            try:
+                user = User.objects.get(email=email)
+                if user.check_password(password):
+                    token, _ = Token.objects.get_or_create(user=user)
+                    return Response({"token": token.key}, status=status.HTTP_200_OK)
+                else:
+                    return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+            except User.DoesNotExist:
+                return Response({"detail": "User not found", "code": "user_not_found"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            token = Token.objects.get(user=request.user)
+            # Delete the token
+            token.delete()
+            return Response({"detail": "Logout successful"}, status=status.HTTP_200_OK)
+        except Token.DoesNotExist:
+            return Response({"detail": "No token found for the user"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class PasswordResetView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        if email:
+            user = User.objects.filter(email=email)
+            if user.exists():
+                user = user.first()
+                token_generator = PasswordResetTokenGenerator()
+                token = token_generator.make_token(user)
+                uidb64 = urlsafe_base64_encode(str(user.pk).encode())
+                protocol = 'https' if request.is_secure() else 'http'
+                domain = request.get_host()
+                url = f'{protocol}://{domain}/api/auth/passwordreset/confirm/{uidb64}/{token}/'
+                return Response({'message': 'Password reset email sent'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({'message': 'Email is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+class PasswordResetConfirmView(APIView):
+    def post(self, request, uidb64, token):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(pk=int(uid))
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user is not None and PasswordResetTokenGenerator().check_token(user, token):
+            password1 = request.data.get('password1')
+            password2 = request.data.get('password2')
+            if password1 and password2:
+                if password1!= password2:
+                    return Response({'message': 'Passwords do not match'}, status=status.HTTP_400_BAD_REQUEST)
+                user.set_password(password1)
+                user.save()
+                return Response({'message': 'Password reset successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'Passwords are required'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'message': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
